c7282b77c554750ffeb488a38bfd3d6b40bd9619
Aditya Dutta <Aditya.Dutta@ivycomptech.com>
2024-08-18 16:02:06 +0530

    command design

---
+++ src/com/example/commands/CopyCommand.java
@@ -0,0 +1,16 @@
+package com.example.commands;
+
+import com.example.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
